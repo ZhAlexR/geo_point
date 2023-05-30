34875691f486815ca424a9986325aae688cdb0d1
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-05-30 19:43:41 +0300

    Added duplication check

--- geo_service/models.py
+++ geo_service/models.py
@@ -1,10 +1,16 @@
 from django.contrib.gis.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Place(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     geom = models.PointField()
+
+    def save(self, *args, **kwargs):
+        if Place.objects.filter(geom=self.geom).exists():
+            raise ValidationError("A place with the same coordinates already exists.")
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.name
